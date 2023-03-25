03fd9a96637af25f59055d14dee728b736e9ad27
RGV <ricardo.guzman.velasco@outlook.com>
2023-03-25 11:08:54 +0100

    feat: raise a building

---
+++ Assets/Softown/Runtime/Infrastructure/Building.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Softown.Runtime.Infrastructure
+{
+    public class Building : MonoBehaviour
+    {
+        public void Raise(int floors, int foundationsWidth)
+        {
+            transform.localScale = new Vector3(foundationsWidth, floors, foundationsWidth);
+        }
+
+        public void Raise(int floors)
+        {
+            Raise(floors, 1);
+        }
+    }
+}
---
+++ Assets/Softown/Tests/Runtime/RaiseBuildingTests.cs
@@ -0,0 +1,49 @@
+using FluentAssertions;
+using NUnit.Framework;
+using Softown.Runtime.Infrastructure;
+using UnityEngine;
+
+namespace Softown.Tests.Runtime
+{
+    public class RaiseBuildingTests
+    {
+        Building sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            var building = new GameObject("", typeof(Building));
+            sut = building.GetComponent<Building>();
+        }
+
+        [Test]
+        public void Building_Cannot_HaveScale0()
+        {
+            sut.transform.localScale.y.Should().BePositive();
+        }
+
+        [Test]
+        public void Raise_ABuilding_With_2Floors()
+        {
+            sut.Raise(floors: 2);
+
+            sut.transform.localScale.y.Should().Be(2);
+        }
+
+        [Test]
+        public void Building_Cannot_HaveFoundations_WithScale0()
+        {
+            sut.transform.localScale.x.Should().BePositive();
+            sut.transform.localScale.z.Should().BePositive();
+        }
+
+        [Test]
+        public void Raise_ABuilding_With_ADifferent_Foundation()
+        {
+            sut.Raise(floors: 1, foundationsWidth: 2);
+
+            sut.transform.localScale.x.Should().Be(2);
+            sut.transform.localScale.z.Should().Be(2);
+        }
+    }
+}

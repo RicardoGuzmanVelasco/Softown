61756fe8d00dd9cb74b12afb6bc77b6fccffa3dc
RGV <ricardo.guzman.velasco@outlook.com>
2023-03-25 10:43:58 +0100

    build: create package layout

---
+++ Assets/Softown/Runtime/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Softown")]
+
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: InternalsVisibleTo("Softown.Tests")]
+
+[assembly: InternalsVisibleTo("Softown.Editor")]
+
+[assembly: InternalsVisibleTo("Softown.Editor.Tests")]

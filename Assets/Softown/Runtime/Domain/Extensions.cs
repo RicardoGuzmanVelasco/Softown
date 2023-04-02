eccd1e0c6b7f57232b0410af322206f3dcb9485b
RGV <ricardo.guzman.velasco@outlook.com>
2023-04-02 12:37:46 +0200

    feat: work with subnamespaces

---
+++ Assets/Softown/Runtime/Domain/Extensions.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Softown.Runtime.Domain
+{
+    public static class Extensions
+    {
+        public static ISet<string> AllNamespaces(this Assembly assembly)
+        {
+            return new HashSet<string>(assembly.GetTypes().Select(t => t.Namespace));
+        }
+
+        public static ISet<string> OnlyRoots(this IEnumerable<string> namespaces)
+        {
+            var global = namespaces.Where(n => n is null);
+            var others = namespaces.Where(n => n is not null).Select(n => n.Split('.').First());
+            return new HashSet<string>(global.Concat(others));
+        }
+
+        public static string TrunkNamespaceRoot(this Type t)
+        {
+            if(t.Namespace is null)
+                return NamespaceSummary.GlobalNamespace;
+
+            if(!t.Namespace.Contains('.'))
+                return string.Empty;
+
+            return t.Namespace[(1+t.Namespace.IndexOf('.'))..];
+        }
+        
+        public static bool IsSubnamespaceOf(this string ns, string root)
+        {
+            if(ns is null)
+                return root is null;
+            if(root is null)
+                return false;
+
+            return ns != root && ns.StartsWith(root);
+        }
+    }
+}
+
+namespace A.B1.C
+{
+    public class C1 { }
+}
+
+namespace A.B2.C
+{
+    public class C1 { }
+}
+
+namespace A.B1
+{
+    public class B11 { }
+}
+
+namespace A
+{
+    public class A1 { }
+}
+
+public class NoNamespace { }
--- Assets/Softown/Tests/Editor/SummarizeNamespaceTests.cs
+++ Assets/Softown/Tests/Editor/SummarizeNamespaceTests.cs
@@ -1,6 +1,7 @@
 ﻿using FluentAssertions;
 using NUnit.Framework;
 using Softown.Runtime.Domain;
+using static Softown.Runtime.Domain.NamespaceSummary;
 
 namespace Softown.Tests.Editor
 {
@@ -14,14 +15,69 @@ namespace Softown.Tests.Editor
                 new[] { null, "A", "A.B1", "A.B1.C", "A.B2.C" }
             );
         }
-        
+
         [Test]
         public void Obtain_OnlyRootNamespaces()
         {
             typeof(A.B1.C.C1).Assembly.AllNamespaces().OnlyRoots().Should().Contain
             (
                 new[] { null, "A" }
-            ).And.NotContain(new[]{"A.B1", "A.B1.C", "A.B2.C"});
+            ).And.NotContain(new[] { "A.B1", "A.B1.C", "A.B2.C" });
+        }
+
+        [Test]
+        public void Trunk_NamespaceRoot_Of_aType()
+        {
+            typeof(A.A1).TrunkNamespaceRoot().Should().Be(string.Empty);
+            typeof(A.B1.C.C1).TrunkNamespaceRoot().Should().Be("B1.C");
+            typeof(NoNamespace).TrunkNamespaceRoot().Should().Be(GlobalNamespace);
+        }
+
+        [Test]
+        public void SubnamespaceOf()
+        {
+            "A".IsSubnamespaceOf("A").Should().BeFalse();
+            "A".IsSubnamespaceOf("B").Should().BeFalse();
+            "A".IsSubnamespaceOf("A.B").Should().BeFalse();
+            "A.B".IsSubnamespaceOf("A").Should().BeTrue();
+            "A.B".IsSubnamespaceOf("A.B").Should().BeFalse();
+            "A.B".IsSubnamespaceOf("A.B.C").Should().BeFalse();
+            "A.B.C".IsSubnamespaceOf("A.B").Should().BeTrue();
+            "A.B.C".IsSubnamespaceOf("A.B.C").Should().BeFalse();
+            "A.B.C".IsSubnamespaceOf("A.B.C.D").Should().BeFalse();
+            "A.B.C.D".IsSubnamespaceOf("A.B.C").Should().BeTrue();
+            "A.B.C.D".IsSubnamespaceOf("A.B.C.D").Should().BeFalse();
+        }
+
+        [Test]
+        public void CannotStore_Type_WithOtherNamespace()
+        {
+            new NamespaceSummary("NO", new[] { typeof(A.B1.C.C1) }).Should().BeEmpty();
+        }
+
+        [Test]
+        public void Store_Type_WithSameNamespace()
+        {
+            new NamespaceSummary("A.B1", new[] { typeof(A.B1.C.C1) }).Should().HaveCount(1);
+        }
+
+        [Test]
+        public void ClassInGlobal()
+        {
+            new NamespaceSummary(GlobalNamespace, new[] { typeof(NoNamespace) }).Should().HaveCount(1);
+        }
+
+        [Test]
+        public void ClassInNamespace()
+        {
+            new NamespaceSummary("A", new[] { typeof(A.A1) }).Classes.Should().Be(1);
+            new NamespaceSummary("A.B1", new[] { typeof(A.A1), typeof(A.B1.B11) }).Classes.Should().Be(1);
+        }
+
+        [Test]
+        public void NamespacesInRoot()
+        {
+            new NamespaceSummary("A", new[] { typeof(A.A1), typeof(A.B1.B11) }).Namespaces.Should().HaveCount(1);
         }
     }
 }

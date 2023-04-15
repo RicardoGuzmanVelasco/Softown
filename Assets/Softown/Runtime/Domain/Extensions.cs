c7b45d42610e8f32eb1b5f109a6f3f03524740eb
RGV <ricardo.guzman.velasco@outlook.com>
2023-04-15 12:19:51 +0200

    test: move placeholder classes to its own assembly, rename to Testown

--- Assets/Softown/Runtime/Domain/Extensions.cs
+++ Assets/Softown/Runtime/Domain/Extensions.cs
@@ -106,25 +106,3 @@ namespace Softown.Runtime.Domain
         }
     }
 }
-
-namespace A.B1.C
-{
-    public class C1 { }
-}
-
-namespace A.B2.C
-{
-    public class C1 { }
-}
-
-namespace A.B1
-{
-    public class B11 { }
-}
-
-namespace A
-{
-    public class A1 { }
-}
-
-public class NoNamespace { }
---
+++ Assets/Softown/Tests/TestAPI/Testown/PlaceholderClasses.cs
@@ -0,0 +1,21 @@
+﻿namespace A.B1.C
+{
+    public class C1 { }
+}
+
+namespace A.B2.C
+{
+    public class C1 { }
+}
+
+namespace A.B1
+{
+    public class B11 { }
+}
+
+namespace A
+{
+    public class A1 { }
+}
+
+public class NoNamespace { }

07e9c1c9988683511555384ddd8a7e85b42ba2ce
RGV <ricardo.guzman.velasco@outlook.com>
2023-04-21 23:32:41 +0200

    feat: visualize some assemblies by menu item instead of scene test

---
+++ Assets/Softown/Editor/VisualizeSomeAssembliesEntryPoint.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Softown.Runtime.Domain;
+using Softown.Runtime.Infrastructure;
+using UnityEngine;
+
+namespace Softown.Editor
+{
+    public class VisualizeSomeAssembliesEntryPoint
+    {
+        [UnityEditor.MenuItem("Softown/Raise/C#")]
+        static void RaiseCSharp()
+        {
+            var assembly = typeof(string).Assembly;
+            Raise(assembly);
+        }
+        
+        [UnityEditor.MenuItem("Softown/Raise/UnityEngine")]
+        static void RaiseUnityEngine()
+        {
+            var assembly = typeof(UnityEngine.MonoBehaviour).Assembly;
+            Raise(assembly);
+        }
+        
+        [UnityEditor.MenuItem("Softown/Raise/UnityEditor")]
+        static void RaiseUnityEditor()
+        {
+            var assembly = typeof(UnityEditor.Editor).Assembly;
+            Raise(assembly);
+        }
+        
+        [UnityEditor.MenuItem("Softown/Raise/Softown")]
+        static void RaiseSoftown()
+        {
+            var assembly = typeof(AssemblySummary).Assembly;
+            Raise(assembly);
+        }
+        
+        static async void Raise(System.Reflection.Assembly assembly)
+        {
+            UnityEditor.EditorApplication.EnterPlaymode();
+            await Task.Delay(TimeSpan.FromSeconds(.5f));
+            
+            var summary = new AssemblySummary(assembly);
+            SpawnVisualizer(summary);
+        }
+
+        static void SpawnVisualizer(AssemblySummary summary)
+        {
+            new GameObject("", typeof(AssemblyVisualizer))
+                .GetComponent<AssemblyVisualizer>()
+                .Visualize(summary);
+        }
+    }
+}
---
+++ Assets/Softown/Runtime/Infrastructure/AssemblyVisualizer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Linq;
+using Softown.Runtime.Domain;
+using UnityEngine;
+
+namespace Softown.Runtime.Infrastructure
+{
+    public class AssemblyVisualizer : MonoBehaviour
+    {
+        bool skipOrdered = false;
+        
+        public void Visualize(AssemblySummary summary)
+        {
+            StartCoroutine(VisualizeSkippingOneAtATime(summary));
+        }
+
+        IEnumerator VisualizeSkippingOneAtATime(AssemblySummary summary)
+        {
+            for(var skipped = 0; skipped < summary.AllContainedClasses.Count(); skipped++)
+            {
+                var sut = new GameObject("", typeof(Neighbourhood)).GetComponent<Neighbourhood>();
+
+                var urbanPlanning = new Architect().Design(summary, skipped);
+                sut.Raise(urbanPlanning);
+
+                yield return new WaitUntil(() => skipOrdered); //TODO: usar unitask y un tcs.
+                skipOrdered = false;
+                Object.Destroy(sut.gameObject);
+            }
+        }
+
+        void Update()
+        {
+            if(Input.anyKeyDown)
+                skipOrdered = true;
+        }
+    }
+}

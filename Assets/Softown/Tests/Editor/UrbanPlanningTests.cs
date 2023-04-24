20cb9508c057eccf59a5835d6ad1daed7af23c53
RGV <ricardo.guzman.velasco@outlook.com>
2023-04-24 20:51:39 +0200

    test: fix urban planning fixture that assumed a blueprint is repeatable
    Ahora un blueprint no puede repetirse a lo largo del urban planning, ya que eso significaría una regresión por estar duplicando clases representadas en edificios.

--- Assets/Softown/Tests/Editor/UrbanPlanningTests.cs
+++ Assets/Softown/Tests/Editor/UrbanPlanningTests.cs
@@ -1,4 +1,4 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
 using FluentAssertions;
 using NUnit.Framework;
 using Softown.Runtime.Domain;
@@ -10,21 +10,36 @@ namespace Softown.Tests.Editor
         [Test]
         public void Obtain_BuildingsAmount()
         {
-            new UrbanPlanning("Whatever", Enumerable.Repeat(new Blueprint(1, 1), 20))
+            new UrbanPlanning("Whatever", 20.Blueprints())
                 .Buildings
                 .Should().Be(20);
 
-            new UrbanPlanning("Whatever", new District[] { new(Enumerable.Repeat(new Blueprint(1, 1), 20)) })
+            new UrbanPlanning("Whatever", new District[] { new(20.Blueprints()) })
                 .Buildings
                 .Should().Be(20);
         }
-        
+
         [Test]
         public void Obtain_BuildingsAmount_Of2Districts()
         {
-            new UrbanPlanning("Whatever", new District[] { new(Enumerable.Repeat(new Blueprint(1, 1), 20)), new(Enumerable.Repeat(new Blueprint(1, 1), 20)) })
+            new UrbanPlanning("Whatever", new District[]
+                {
+                    new(20.Blueprints("FirstDistrict")),
+                    new(20.Blueprints("SecondDistrict"))
+                })
                 .Buildings
                 .Should().Be(40);
+        }
+    }
+
+    public static class BlueprintExtensions
+    {
+        public static IEnumerable<Blueprint> Blueprints(this int howMany, string name = "Blueprint")
+        {
+            var blueprints = new List<Blueprint>();
+            for(var i = 1; i <= howMany; i++)
+                blueprints.Add(new($"{name}{i}", 1, 1));
+            return blueprints;
         }
     }
 }
